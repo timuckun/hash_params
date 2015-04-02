f8c6228bc171e423495fdab969f8bc1e958b1feb
Tim Uckun <tim@uckun.com>
2015-04-02 16:01:39 +1300

    fixed the recursive problem

--- lib/hash_params.rb
+++ lib/hash_params.rb
@@ -1,11 +1,12 @@
+<<<<<<< HEAD
 require 'yaml'
 require 'erb'
 require 'socket'
 
-
 require_relative 'hash_params/hash_validator'
 require_relative 'hash_params/validator'
 require_relative 'hash_params/binding_validator'
+
 module HashParams
   ENVIRONMENT =  ENV['HASH_PARAMS_ENV'] || (defined?(Rails) && Rails.env) || ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
   VERSION = '2.0.0'
